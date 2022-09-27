bcb8b70377adfbb5d4795986f102dc624eaa2252
xiaofan0408 <1002758393@qq.com>
2022-09-27 17:52:19 +0800

    提交sqlparser代码

--- src/main/java/com/xiaofan0408/parser/SqlParser.java
+++ src/main/java/com/xiaofan0408/parser/SqlParser.java
@@ -1,4 +1,26 @@
 package com.xiaofan0408.parser;
 
+import com.xiaofan0408.parser.antlr.SqlBaseLexer;
+import com.xiaofan0408.parser.antlr.SqlBaseParser;
+import com.xiaofan0408.parser.operate.OperateBase;
+import org.antlr.v4.runtime.CharStream;
+import org.antlr.v4.runtime.CharStreams;
+import org.antlr.v4.runtime.CommonTokenStream;
+import org.antlr.v4.runtime.tree.ParseTree;
+
 public class SqlParser {
+
+    private SqlVisitor sqlVisitor = new SqlVisitor();
+
+    public OperateBase parseSql(String database,String sql){
+        CharStream input = CharStreams.fromString(sql);
+        SqlBaseLexer lexer = new SqlBaseLexer(input);
+        CommonTokenStream tokens = new CommonTokenStream(lexer);
+        SqlBaseParser parser = new SqlBaseParser(tokens);
+        ParseTree parseTree = parser.singleStatement();
+        OperateBase operateBase = sqlVisitor.visit(parseTree);
+        operateBase.setDatabase(database);
+        return operateBase;
+    }
+
 }
