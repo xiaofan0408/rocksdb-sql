00ee799958e3491fa14e5dab2688fbcfdf7323b0
xiaofan0408 <1002758393@qq.com>
2022-09-27 11:38:36 +0800

    提交对外方法

--- src/main/java/com/xiaofan0408/RocksDbSql.java
+++ src/main/java/com/xiaofan0408/RocksDbSql.java
@@ -1,13 +1,34 @@
 package com.xiaofan0408;
 
+import com.xiaofan0408.base.Result;
+import com.xiaofan0408.executor.SqlExecutor;
+import com.xiaofan0408.parser.SqlParser;
+import com.xiaofan0408.parser.operate.OperateBase;
+import com.xiaofan0408.plan.SqlPlanner;
+import com.xiaofan0408.plan.model.Plan;
 import com.xiaofan0408.storage.Storage;
 
 public class RocksDbSql {
 
     private Storage storage;
 
+    private SqlExecutor sqlExecutor;
+
+    private SqlPlanner sqlPlanner;
+
+    private SqlParser sqlParser;
+
     public RocksDbSql(String path){
         storage = new Storage(path);
+        sqlParser = new SqlParser();
+        sqlPlanner = new SqlPlanner();
+        sqlExecutor = new SqlExecutor();
+    }
+
+    public Result execute(String database, String sql){
+        OperateBase operateBase = sqlParser.parseSql(database,sql);
+        Plan plan = sqlPlanner.plan(operateBase);
+        return sqlExecutor.execute(plan);
     }
 
 }
