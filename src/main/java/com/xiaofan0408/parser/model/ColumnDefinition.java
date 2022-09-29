62828727602ffef87f71fea96100add31dd8af0f
xiaofan0408 <1002758393@qq.com>
2022-09-29 07:40:15 +0000

    提交createTable结构

---
+++ src/main/java/com/xiaofan0408/parser/model/ColumnDefinition.java
@@ -0,0 +1,21 @@
+package com.xiaofan0408.parser.model;
+
+public class ColumnDefinition {
+    
+    private String columnType;
+
+    private String columnName;
+
+    public ColumnDefinition(String type,String name){
+        this.columnType = type;
+        this.columnName = name;
+    }
+
+    public String getColumnName() {
+        return columnName;
+    }
+    
+    public String getColumnType() {
+        return columnType;
+    }
+}
--- src/main/java/com/xiaofan0408/parser/operate/CreateTable.java
+++ src/main/java/com/xiaofan0408/parser/operate/CreateTable.java
@@ -1,17 +1,36 @@
 package com.xiaofan0408.parser.operate;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import com.xiaofan0408.parser.model.ColumnDefinition;
+
 public class CreateTable extends OperateBase{
 
     private String tableName;
  
-     
-   public void setTableName(String tableName){
-       this.tableName = tableName;
-   }
+    private List<ColumnDefinition> columns;
 
-   public String getTableName(){
+    public CreateTable(){
+        columns = new ArrayList<>();
+    }
+      
+    public void setTableName(String tableName){
+     this.tableName = tableName;
+    }
+
+    public String getTableName(){
        return this.tableName;
-   }
+    }
+
+    public void setColumns(List<ColumnDefinition> columns) {
+       this.columns.clear();
+       this.columns.addAll(columns);
+    }
+
+    public void addColumn(ColumnDefinition column){
+        this.columns.add(column);
+    }
 
    @Override
    public String toString() {
